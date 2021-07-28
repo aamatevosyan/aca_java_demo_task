f46dc5fd7fa18482c755d1da76feb29f0a113ea9
Armen Matevosyan <aamatevosyan@edu.hse.ru>
2021-07-28 20:26:58 +0400

    chore: Initial commit

---
+++ src/main/java/com/ama/aca_demo_task/AcaDemoTaskApplication.java
@@ -0,0 +1,13 @@
+package com.ama.aca_demo_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcaDemoTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcaDemoTaskApplication.class, args);
+    }
+
+}
